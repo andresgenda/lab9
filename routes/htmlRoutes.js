2e63ebbd74d7273db0c845ce531b64bd319c6aae
andresgenda <54486269+andresgenda@users.noreply.github.com>
2021-05-03 00:00:13 -0700

    Load views using Render

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,29 +4,34 @@
 // ===============================================================================
 var path = require("path");
 
+var tableData = require("../data/tableData");
+var waitListData = require("../data/waitinglistData");
+
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function(app) {
 
-  app.set('views', path.join(__dirname,'views'));
+  //app.set('views', path.join(__dirname,'views'));
   app.set('view engine', 'ejs');
+
   // HTML GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("/tables", function(req, res) {
-    res.render('pages/tables');
+  app.get("/tables", (req, res) => {
+    console.log(tableData);
+    res.render(path.join(__dirname,'../views/pages/tables'),{tableData});
   });
 
-  app.get("/reserve", function(req, res) {
-    res.render('pages/reserve');
+  app.get("/reserve", (req, res) => {
+    res.render(path.join(__dirname,'../views/pages/reserve'));
   });
 
   // If no matching route is found default to home
-  app.get("/", function(req, res) {
-    res.render('pages/home');
+  app.get("/", (req, res) => {
+    res.render(path.join(__dirname,'../views/pages/home'));
   });
 };
