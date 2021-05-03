2d27e03ea9cc72b37dfae13ae53ca9e2804a948f
andresgenda <54486269+andresgenda@users.noreply.github.com>
2021-05-02 22:55:20 -0700

    Commit Changes to HTML Routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,27 +4,29 @@
 // ===============================================================================
 var path = require("path");
 
-
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function(app) {
+
+  app.set('views', path.join(__dirname,'views'));
+  app.set('view engine', 'ejs');
   // HTML GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
   app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
+    res.render('pages/tables');
   });
 
   app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+    res.render('pages/reserve');
   });
 
   // If no matching route is found default to home
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+  app.get("/", function(req, res) {
+    res.render('pages/home');
   });
 };
